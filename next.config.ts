f77ce28b74aa7803253c3462941c20ebc26f8cbf
arun-kumar <gamingrook271@gmail.com>
2026-05-15 09:16:40 +0530

    commit final

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.31.127'],
+  output: "export",
 };
 
 export default nextConfig;
